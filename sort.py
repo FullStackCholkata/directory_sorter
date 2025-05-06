b6a2e34f93cbaa38067df8ec023a48ec6709d071
Lachezar Damyanov <e12319590@student.tuwien.ac.at>
2025-05-06 23:26:27 +0200

    Script finished

--- sort.py
+++ sort.py
@@ -1,12 +1,13 @@
 import os
 import shutil
-import pathlib
 
-target_path = "C:/Users/Owner/Downloads"
+# We specify the target destination and check if it exists
+target_path = "C:\\Users\\Owner\\Downloads"
 if not os.path.exists(target_path):
     print('Path does not exist!')
     exit(0)
 
+# We define categories for files we want to sort
 categories = {
     "System Files" : ['.exe', '.dll', '.sys', '.drv', '.ini', '.bat', '.cmd', '.msi', '.vxd', '.iso'],
     "Documents" : ['.txt', '.docx', '.pdf'],
@@ -21,14 +22,55 @@ categories = {
 
 }
 
+# We will dynamically create folders to which we will put files in later
+created_folders = {}
+
+# Listing the contents of the target directory
 directory_contents = os.listdir(target_path)
+# Creating a data structure to store subfolders
 directories = []
+
+# Only if the directory has contents we check each one if it is a file or a subfolder
 if directory_contents:
     for element in directory_contents:
         path_to_element = os.path.join(target_path, element)
+
         if os.path.isdir(path_to_element):
             directories.append(element)
-        #elif os.path.isfile(path_to_element):
+
+        elif os.path.isfile(path_to_element):
+            file_extension = os.path.splitext(element)[1]
+
+            # We try to check if the selected file matches one of our categories
+            for category in categories:
+                for extension in categories.get(category):
+                    if file_extension == extension:
+
+                        # Here the dynamic dictionary creation takes place:
+                        # The 'category' is going to be used a folder name
+                        # Inside this folder the absolute path to the element in question is saved
+                        if not category in created_folders:
+                            created_folders[category] = []
+                        created_folders[category].append(path_to_element)
+                        break
+else:
+    print('The downloads are empty!')
+    exit(0)
+
+moved_files = 0
+new_folders = 0
+
+# For each dynamically added folder we create an actual directory if it doesn't already exist
+for folder in created_folders:
+
+    if not folder in directories:
+        os.mkdir(os.path.join(target_path, folder))
+        new_folders += 1
+
+    destination_path = os.path.join(target_path, folder)
+    for path in created_folders.get(folder):
+        shutil.move(path, destination_path)
+        moved_files += 1
 
 
-print(os.listdir(target_path))
+print(f'Moving is done! {moved_files} has been moved into {new_folders} new folders!')
